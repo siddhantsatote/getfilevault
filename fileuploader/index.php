658fbd858892fa315a2b159b1b634213792e99a7
siddhantsatote <128237443+siddhantsatote@users.noreply.github.com>
2023-08-26 18:46:53 +0530

    Update index.php

--- fileuploader/index.php
+++ fileuploader/index.php
@@ -67,7 +67,7 @@ $conn->close();
 <body>
     <div class="login-container">
         <h2>Login</h2>
-        <form action="uploadfile.php" method="post">
+        <form action="index.php" method="post">
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Login</button>
